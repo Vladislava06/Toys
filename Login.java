b3d31e5bfdcf3ed407c555b9385a08658347c139
Vladislava <v.ryani@mail.ru>
2023-06-28 22:16:38 +0300

    Сделано

---
+++ Login.java
@@ -0,0 +1,11 @@
+
+import java.io.FileWriter;
+import java.io.IOException;
+public class Login {
+    public void writeToFile(Toy toy) throws IOException {
+        FileWriter writer = new FileWriter("results.txt", true);
+        writer.write(toy.toString());
+        writer.write("\n");
+        writer.flush();
+    }
+}
